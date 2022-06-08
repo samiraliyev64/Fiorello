ccd068fe01d113ed1609c997e6f8a1897d1e3466
samiraliyev64 <70ey3w0@code.edu.az>
2022-06-08 13:11:34 +0400

    update project

--- Fiorello/Areas/AdminPanel/Controllers/CategoryController.cs
+++ Fiorello/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -1,17 +1,106 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fiorello.Models;
+using Fiorello.ViewModels.Categories;
 
 namespace Fiorello.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
     public class CategoryController : Controller
     {
+        private AppDbContext _context { get; }
+        private IEnumerable<Category> categories { get; set; }
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+            categories = _context.Categories.Where(ct => !ct.isDeleted);
+        }
         public IActionResult Index()
         {
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       public async Task<IActionResult> Create(CategoryCreateVM category)
+        {
+            if (!ModelState.IsValid) { return View(); }
+            bool isExist = categories.Where(c => !c.isDeleted).Any(c => c.Name.ToLower() == category.Name.ToLower());
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} does exist");
+                return View();
+            }
+            Category newCategory = new Category { Name = category.Name };
+
+            await _context.Categories.AddAsync(newCategory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+            Category category = _context.Categories.Where(c=>!c.isDeleted).FirstOrDefault(c => c.Id == id );
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id,Category category)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Category categoryDb = _context.Categories.Where(c => !c.isDeleted).FirstOrDefault(c => c.Id == id);
+            if (categoryDb == null)
+            {
+                return NotFound();
+            }
+            //if(categoryDb.Name.ToLower() == category.Name.ToLower())
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            bool isExist = categories.Where(c => !c.isDeleted).Any(c => c.Name.ToLower() == category.Name.ToLower() && c.Id != categoryDb.Id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} does exist");
+                return View();
+            }
+            categoryDb.Name = category.Name;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+            Category categoryDb = _context.Categories.Where(c => !c.isDeleted).FirstOrDefault(c=>c.Id == id);
+            if(categoryDb == null)
+            {
+                return NotFound();
+            }
+            //_context.Categories.Remove(categoryDb);
+            categoryDb.isDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ Fiorello/Areas/AdminPanel/Controllers/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using Fiorello.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorello.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class SliderController : Controller
+    {
+        private AppDbContext _context { get; }
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Slides);
+        }
+    }
+}
